6bb6f222991b21094615849b142594f2d5ededa2
young5454 <nathanyoung54@snu.ac.kr>
2023-10-16 14:45:58 +0900

    Within-group Roary

--- snakefile.py
+++ snakefile.py
@@ -71,6 +71,7 @@ rule all:
         expand("/jupyterdem/annotation/{group}_{strain}/{group}_{strain}.tbl", zip, group=GROUP, strain=STRAIN),
         expand("/jupyterdem/annotation/{group}_{strain}/{group}_{strain}.txt", zip, group=GROUP, strain=STRAIN),
 
+        ### Roary I/O files
         # Roary outputs - STRAIN-to-REF pairwise
         expand("/jupyterdem/pangenome/{group}_{strain}_ref_pairwise/accessory.header.embl", zip, group=GROUP, strain=STRAIN),
         expand("/jupyterdem/pangenome/{group}_{strain}_ref_pairwise/accessory.tab", zip, group=GROUP, strain=STRAIN),
@@ -92,7 +93,55 @@ rule all:
         expand("/jupyterdem/pangenome/{group}_{strain}_ref_pairwise/pan_genome_reference.fa", zip, group=GROUP, strain=STRAIN),
         expand("/jupyterdem/pangenome/{group}_{strain}_ref_pairwise/summary_statistics.txt", zip, group=GROUP, strain=STRAIN),
 
-        expand("/jupyterdem/roary_tmp/{group}/", group=GROUP)
+        # Roary directory - Roary tmp for holding strain GFFs
+        # expand("/jupyterdem/roary_tmp/{group}/", group=GROUP),
+
+        # Roary input - Within-GROUP Roary without reference
+        # expand("/jupyterdem/roary_tmp/{group}/{group}_{strain}.gff", zip, group=GROUP, strain=STRAIN),
+
+        # Roary output - Within-Group Roary output
+        expand("/jupyterdem/pangenome2/{group}", group=GROUP)
+
+        # Roary output - Within-GROUP Roary without reference
+        # expand("/jupyterdem/roary_tmp/{group}/accessory.header.embl", group=GROUP),
+        # expand("/jupyterdem/roary_tmp/{group}/accessory.tab", group=GROUP),
+        # expand("/jupyterdem/roary_tmp/{group}/accessory_binary_genes.fa", group=GROUP),
+        # expand("/jupyterdem/roary_tmp/{group}/accessory_graph.dot", group=GROUP),
+        # expand("/jupyterdem/roary_tmp/{group}/blast_identity_frequency.Rtab", group=GROUP),
+        # expand("/jupyterdem/roary_tmp/{group}/clustered_proteins", group=GROUP),
+        # expand("/jupyterdem/roary_tmp/{group}/core_accessory.header.embl", group=GROUP),
+        # expand("/jupyterdem/roary_tmp/{group}/core_accessory.tab", group=GROUP),
+        # expand("/jupyterdem/roary_tmp/{group}/core_accessory_graph.dot", group=GROUP),
+        # expand("/jupyterdem/roary_tmp/{group}/core_alignment_header.embl", group=GROUP),
+        # expand("/jupyterdem/roary_tmp/{group}/core_gene_alignment.aln", group=GROUP),
+        # expand("/jupyterdem/roary_tmp/{group}/gene_presence_absence.Rtab", group=GROUP),
+        # expand("/jupyterdem/roary_tmp/{group}/gene_presence_absence.csv", group=GROUP),
+        # expand("/jupyterdem/roary_tmp/{group}/number_of_conserved_genes.Rtab", group=GROUP),
+        # expand("/jupyterdem/roary_tmp/{group}/number_of_genes_in_pan_genome.Rtab", group=GROUP),
+        # expand("/jupyterdem/roary_tmp/{group}/number_of_new_genes.Rtab", group=GROUP),
+        # expand("/jupyterdem/roary_tmp/{group}/number_of_unique_genes.Rtab", group=GROUP),
+        # expand("/jupyterdem/roary_tmp/{group}/pan_genome_reference.fa", group=GROUP),
+        # expand("/jupyterdem/roary_tmp/{group}/summary_statistics.txt", group=GROUP)
+
+        # expand("/jupyterdem/pangenome2/{group}_within_group/accessory.header.embl", group=GROUP),
+        # expand("/jupyterdem/pangenome2/{group}_within_group/accessory.tab", group=GROUP),
+        # expand("/jupyterdem/pangenome2/{group}_within_group/accessory_binary_genes.fa", group=GROUP),
+        # expand("/jupyterdem/pangenome2/{group}_within_group/accessory_graph.dot", group=GROUP),
+        # expand("/jupyterdem/pangenome2/{group}_within_group/blast_identity_frequency.Rtab", group=GROUP),
+        # expand("/jupyterdem/pangenome2/{group}_within_group/clustered_proteins", group=GROUP),
+        # expand("/jupyterdem/pangenome2/{group}_within_group/core_accessory.header.embl", group=GROUP),
+        # expand("/jupyterdem/pangenome2/{group}_within_group/core_accessory.tab", group=GROUP),
+        # expand("/jupyterdem/pangenome2/{group}_within_group/core_accessory_graph.dot", group=GROUP),
+        # expand("/jupyterdem/pangenome2/{group}_within_group/core_alignment_header.embl", group=GROUP),
+        # expand("/jupyterdem/pangenome2/{group}_within_group/core_gene_alignment.aln", group=GROUP),
+        # expand("/jupyterdem/pangenome2/{group}_within_group/gene_presence_absence.Rtab", group=GROUP),
+        # expand("/jupyterdem/pangenome2/{group}_within_group/gene_presence_absence.csv", group=GROUP),
+        # expand("/jupyterdem/pangenome2/{group}_within_group/number_of_conserved_genes.Rtab", group=GROUP),
+        # expand("/jupyterdem/pangenome2/{group}_within_group/number_of_genes_in_pan_genome.Rtab", group=GROUP),
+        # expand("/jupyterdem/pangenome2/{group}_within_group/number_of_new_genes.Rtab", group=GROUP),
+        # expand("/jupyterdem/pangenome2/{group}_within_group/number_of_unique_genes.Rtab", group=GROUP),
+        # expand("/jupyterdem/pangenome2/{group}_within_group/pan_genome_reference.fa", group=GROUP),
+        # expand("/jupyterdem/pangenome2/{group}_within_group/summary_statistics.txt", group=GROUP)
 
 
 # Rule to run Polypolish for polishing long-read assemblies with Illumina short reads
@@ -215,70 +264,168 @@ rule roary_strain_ref_pairwise:
     shell:
         """
         cd {params.out_dir}
-        roary -e -p {params.threads} -v {input.ref_gff} {input.strain_gff}
+        roary -e -p {params.threads} -i {params.pident} -v {input.ref_gff} {input.strain_gff}
+        """
+
+## Rule to make roary_tmp group folder
+
+# Rule to run Roary for STRAINS within GROUPS
+rule roary_within_group:
+    # input:
+    #     # Input GFFs are annotated GFFs from Prokka
+    #     strain_gff=expand("/jupyterdem/roary_tmp/{group}/{group}_{strain}.gff", zip, group=GROUP, strain=STRAIN)
+    output:
+        out_dir=directory("/jupyterdem/pangenome2/{group}")
+    params:
+        threads=8,
+        tmp_dir=directory("/jupyterdem/roary_tmp/{group}/"),
+        pident=90
+    shell:
+        """
+        mkdir {output.out_dir}
+        cd {params.tmp_dir}
+        roary -e -p {params.threads} -i {params.pident} -v *.gff
+        mv *.gff {output.out_dir}
         """
 
 
-# Rule to copy and save STRAIN GFFs into a tmp folder
-rule copy_strain_gffs:
-    input:
-        strain_gff=expand("/jupyterdem/annotation/{group}_{strain}/{group}_{strain}.gff", zip, group=GROUP, strain=STRAIN)
-    output:
-        out_dir=directory("/jupyterdem/roary_tmp/{group}/")
-    run:
-        # Part 1: Execute before the if statement
-        # print(type(input.strain_gff))
-        input_strin_gff_list = str(input.strain_gff).split(' ')
-        print(input_strin_gff_list)
-        shell("mkdir -p {output.out_dir}")
-        # Check if the 'GROUP' part of the 'strain_gff' path matches the 'group' in 'out_dir'
-        for strain in input_strin_gff_list:
-            group_id = strain.split("/")[-1].split('_')[0]
-            print(group_id)
-            if group_id == output.out_dir.split("/")[-1]:
-                # Part 2: Execute if the if statement is true
-                shell("cp {strain} {output.out_dir}")
+# # Rule to copy and save STRAIN GFFs into a tmp folder for within-group Roary
+# rule copy_strain_gffs:
+#     input:
+#         strain_gff=expand("/jupyterdem/annotation/{group}_{strain}/{group}_{strain}.gff", zip, group=GROUP, strain=STRAIN)
+#     output:
+#         out_dir=directory("/jupyterdem/roary_tmp/{group}/")
+#     run:
+#         input_string_gff_list = str(input.strain_gff).split(' ')
+#         print(input_string_gff_list)
+#         # Create GROUP directories
+#         shell("mkdir -p {output.out_dir}")
+#         # Check if the 'GROUP' part of the 'strain_gff' path matches the {group} in 'out_dir'
+#         for strain in input_string_gff_list:
+#             group_id = strain.split("/")[-1].split('_')[0]
+#             if group_id == output.out_dir.split("/")[-1]:
+#                 # Part 2: Execute if the if statement is true
+#                 shell("cp {strain} {output.out_dir}")
+
+
+# def list_files_in_directory(directory_path):
+#     try:
+#         # Get a list of all files in the specified directory
+#         files = os.listdir(directory_path)
+        
+#         # Create a string with each file on a new line
+#         file_list_string = " ".join(files)
+
+#         return file_list_string
+
+#     except Exception as e:
+#         return str(e)
+
+# # Example usage:
+# directory_path = "/jupyterdem/roary_tmp/hvCRAB/"
+# file_list = list_files_in_directory(directory_path)
+
+# print(file_list)
+
+# rule roary_within_group:
+#     input:
+#         expand("/jupyterdem/roary_tmp/{group}/{group}_{strain}.gff", zip, group=GROUP, strain=STRAIN)
+#     output:
+#         "roary.done"
+#     params:
+#         threads=8,
+#         pident=90,
+#         work_dir="/jupyterdem/roary_tmp/{group}/",
+#         out_dir="/jupyterdem/pangenome2/{group}/"
+#     shell:
+#         """
+#         cd {params.work_dir}
+#         roary -e -p {params.threads} -f {params.out_dir} -i {params.pident} -v {input} && touch {output}
+#         """
+
+
+# rule copy_strain_gffs:
+#     input:
+#         strain_gff=expand("/jupyterdem/annotation/{group}_{strain}/{group}_{strain}.gff", zip, group=GROUP, strain=STRAIN)
+#     output:
+#         out_dir=directory("/jupyterdem/roary_tmp/{group}/")
+#     shell:
+#         """
+#         mkdir {output.out_dir}
+#         cp {input.strain_gff} {output.out_dir}
+#         """
+
+
+
+    # run:
+    #     shell("cd {params.tmp_dir}")
+    #     shell("roary -e -p {params.threads} -i {params.pident} -v *.gff")
+    #     shell("mv *.gff {output.out_dir}")
+    # output:
+    #     accessory_header="/jupyterdem/roary_tmp/{group}/accessory.header.embl",
+    #     accessory_tab="/jupyterdem/roary_tmp/{group}/accessory.tab",
+    #     accessory_binary_genes="/jupyterdem/roary_tmp/{group}/accessory_binary_genes.fa",
+    #     accessory_graph="/jupyterdem/roary_tmp/{group}/accessory_graph.dot",
+    #     blast_identity_frequency="/jupyterdem/roary_tmp/{group}/blast_identity_frequency.Rtab",
+    #     clustered_proteins="/jupyterdem/roary_tmp/{group}/clustered_proteins",
+    #     core_accessory_header="/jupyterdem/roary_tmp/{group}/core_accessory.header.embl",
+    #     core_accessory="/jupyterdem/roary_tmp/{group}/core_accessory.tab",
+    #     core_accessory_graph="/jupyterdem/roary_tmp/{group}/core_accessory_graph.dot",
+    #     core_alignment_header="/jupyterdem/roary_tmp/{group}/core_alignment_header.embl",
+    #     core_gene_alignment="/jupyterdem/roary_tmp/{group}/core_gene_alignment.aln",
+    #     gene_presence_absence_rtab="/jupyterdem/roary_tmp/{group}/gene_presence_absence.Rtab",
+    #     gene_presence_absence_csv="/jupyterdem/roary_tmp/{group}/gene_presence_absence.csv",
+    #     number_of_conserved_genes="/jupyterdem/roary_tmp/{group}/number_of_conserved_genes.Rtab",
+    #     number_of_genes_in_pan_genome="/jupyterdem/roary_tmp/{group}/number_of_genes_in_pan_genome.Rtab",
+    #     number_of_new_genes="/jupyterdem/pangenome2/roary_tmp/{group}/number_of_new_genes.Rtab",
+    #     number_of_unique_genes="/jupyterdem/pangenome2/roary_tmp/{group}/number_of_unique_genes.Rtab",
+    #     pan_genome_reference="/jupyterdem/pangenome2/roary_tmp/{group}/pan_genome_reference.fa",
+    #     summary_statistics="/jupyterdem/pangenome2/roary_tmp/{group}/summary_statistics.txt"
+    # params:
+    #     threads=8,
+    #     out_dir="/jupyterdem/roary_tmp/{group}/",
+    #     pident=90
     # shell:
     #     """
-    #     mkdir -p {output[0]}
-    #     cp {input.strain_gff} {output.out_dir}
+    #     cd {params.out_dir}
+    #     roary -e -p {params.threads} -i {params.pident} -v *.gff
     #     """
 
-# # Rule to run Roary for STRAIN-to-REF 1:1 pairwise Pangenome analysis
+
 # rule roary_within_group:
 #     input:
 #         # Input GFFs are annotated GFFs from Prokka
-#         strain_gff="/jupyterdem/annotation/{group}_{strain}/{group}_{strain}.gff"
+#         strain_gff=expand("/jupyterdem/roary_tmp/{group}/{group}_{strain}.gff", zip, group=GROUP, strain=STRAIN)
 #     output:
-#         accessory_header="/jupyterdem/pangenome/{group}_{strain}_ref_pairwise/accessory.header.embl",
-#         accessory_tab="/jupyterdem/pangenome/{group}_{strain}_ref_pairwise/accessory.tab",
-#         accessory_binary_genes="/jupyterdem/pangenome/{group}_{strain}_ref_pairwise/accessory_binary_genes.fa",
-#         accessory_graph="/jupyterdem/pangenome/{group}_{strain}_ref_pairwise/accessory_graph.dot",
-#         blast_identity_frequency="/jupyterdem/pangenome/{group}_{strain}_ref_pairwise/blast_identity_frequency.Rtab",
-#         clustered_proteins="/jupyterdem/pangenome/{group}_{strain}_ref_pairwise/clustered_proteins",
-#         core_accessory_header="/jupyterdem/pangenome/{group}_{strain}_ref_pairwise/core_accessory.header.embl",
-#         core_accessory="/jupyterdem/pangenome/{group}_{strain}_ref_pairwise/core_accessory.tab",
-#         core_accessory_graph="/jupyterdem/pangenome/{group}_{strain}_ref_pairwise/core_accessory_graph.dot",
-#         core_alignment_header="/jupyterdem/pangenome/{group}_{strain}_ref_pairwise/core_alignment_header.embl",
-#         core_gene_alignment="/jupyterdem/pangenome/{group}_{strain}_ref_pairwise/core_gene_alignment.aln",
-#         gene_presence_absence_rtab="/jupyterdem/pangenome/{group}_{strain}_ref_pairwise/gene_presence_absence.Rtab",
-#         gene_presence_absence_csv="/jupyterdem/pangenome/{group}_{strain}_ref_pairwise/gene_presence_absence.csv",
-#         number_of_conserved_genes="/jupyterdem/pangenome/{group}_{strain}_ref_pairwise/number_of_conserved_genes.Rtab",
-#         number_of_genes_in_pan_genome="/jupyterdem/pangenome/{group}_{strain}_ref_pairwise/number_of_genes_in_pan_genome.Rtab",
-#         number_of_new_genes="/jupyterdem/pangenome/{group}_{strain}_ref_pairwise/number_of_new_genes.Rtab",
-#         number_of_unique_genes="/jupyterdem/pangenome/{group}_{strain}_ref_pairwise/number_of_unique_genes.Rtab",
-#         pan_genome_reference="/jupyterdem/pangenome/{group}_{strain}_ref_pairwise/pan_genome_reference.fa",
-#         summary_statistics="/jupyterdem/pangenome/{group}_{strain}_ref_pairwise/summary_statistics.txt"
+#         accessory_header="/jupyterdem/pangenome2/{group}_within_group/accessory.header.embl",
+#         accessory_tab="/jupyterdem/pangenome2/{group}_within_group/accessory.tab",
+#         accessory_binary_genes="/jupyterdem/pangenome2/{group}_within_group/accessory_binary_genes.fa",
+#         accessory_graph="/jupyterdem/pangenome2/{group}_within_group/accessory_graph.dot",
+#         blast_identity_frequency="/jupyterdem/pangenome2/{group}_within_group/blast_identity_frequency.Rtab",
+#         clustered_proteins="/jupyterdem/pangenome2/{group}_within_group/clustered_proteins",
+#         core_accessory_header="/jupyterdem/pangenome2/{group}_within_group/core_accessory.header.embl",
+#         core_accessory="/jupyterdem/pangenome2/{group}_within_group/core_accessory.tab",
+#         core_accessory_graph="/jupyterdem/pangenome2/{group}_within_group/core_accessory_graph.dot",
+#         core_alignment_header="/jupyterdem/pangenome2/{group}_within_group/core_alignment_header.embl",
+#         core_gene_alignment="/jupyterdem/pangenome2/{group}_within_group/core_gene_alignment.aln",
+#         gene_presence_absence_rtab="/jupyterdem/pangenome2/{group}_within_group/gene_presence_absence.Rtab",
+#         gene_presence_absence_csv="/jupyterdem/pangenome2/{group}_within_group/gene_presence_absence.csv",
+#         number_of_conserved_genes="/jupyterdem/pangenome2/{group}_within_group/number_of_conserved_genes.Rtab",
+#         number_of_genes_in_pan_genome="/jupyterdem/pangenome2/{group}_within_group/number_of_genes_in_pan_genome.Rtab",
+#         number_of_new_genes="/jupyterdem/pangenome2/{group}_within_group/number_of_new_genes.Rtab",
+#         number_of_unique_genes="/jupyterdem/pangenome2/{group}_within_group/number_of_unique_genes.Rtab",
+#         pan_genome_reference="/jupyterdem/pangenome2/{group}_within_group/pan_genome_reference.fa",
+#         summary_statistics="/jupyterdem/pangenome2/{group}_within_group/summary_statistics.txt"
 #     params:
 #         threads=8,
+#         work_dir="/jupyterdem/roary_tmp/{group}/",
 #         out_dir="/jupyterdem/pangenome2/{group}_within_group/",
 #         pident=90
 #     shell:
 #         """
-#         cd {params.out_dir}
-#         roary -e -p {params.threads} -v {input.ref_gff} {input.strain_gff}
+#         cd {params.work_dir}
+#         roary -f {params.out_dir} -e -p {params.threads} -i {params.pident} -v *.gff
 #         """
-
 
 # # Rule to run ABRicate for annotating virulence factors within REFERENCE FASTAs
 # rule abricate_ref:
